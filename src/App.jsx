2e5d92df3aa36041054455cb428dd9178a5829ce
Franco Borghi <francox93@hotmail.com>
2024-01-10 18:20:14 -0300

    Franco commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,51 @@
-import React from "react";
-import Buttonpanel from "./components/Buttonpanel";
-import NavBar from "./components/NavBar";
-import Insurrance from "./components/Insurrance";
-import About from "./components/About";
-import Carousel from "./components/Carousel";
-import Contact from "./components/Contact";
-import Companies from "./components/Companies";
-import Footer from "./components/Footer";
-import FAB from "./components/Fab";
+import React from 'react';
+import Buttonpanel from './components/Buttonpanel';
+import NavBar from './components/NavBar';
+import Insurrance from './components/Insurrance';
+import About from './components/About';
+import Carousel from './components/Carousel';
+import Contact from './components/Contact';
+import Companies from './components/Companies';
+import Footer from './components/Footer';
+import FAB from './components/Fab';
+import NavBarMobile from './components/NavBarMobile';
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Carousel />
-      <section
-        id="insurrance"
-        style={{
-          margin: "0",
-        }}
-      >
-        <Buttonpanel />
-      </section>
-      <FAB />
-      <section
-        id="about"
-        style={{
-          margin: "0",
-        }}
-      >
-        <About />
-        <Insurrance />
-        <Companies />
-      </section>
-      <section
-        id="contact"
-        style={{
-          margin: "0",
-        }}
-      >
-        <Contact />
-      </section>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<NavBarMobile />
+			<Carousel />
+			<section
+				id='insurrance'
+				style={{
+					margin: '0',
+				}}
+			>
+				<Buttonpanel />
+			</section>
+			<FAB />
+			<section
+				id='about'
+				style={{
+					margin: '0',
+				}}
+			>
+				<About />
+				<Insurrance />
+				<Companies />
+			</section>
+			<section
+				id='contact'
+				style={{
+					margin: '0',
+				}}
+			>
+				<Contact />
+			</section>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
